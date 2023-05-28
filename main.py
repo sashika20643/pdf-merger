aa9efd5c70c9608cf53e7987a639c710b8ec987f
sashika20643 <sashikadulaj20643@gmail.com>
2023-05-28 20:10:16 +0530

    merger function added

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/merge', methods=['POST'])
+def merge():
+    # Get uploaded files
+    uploaded_files = request.files.getlist('pdf_files')
+
+    merger = PyPDF2.PdfMerger()
+    for file in uploaded_files:
+        merger.append(file)
+
+    # Generate unique filename for merged PDF
+    output_filename = 'merged.pdf'
+    i = 1
+    while os.path.exists(output_filename):
+        output_filename = f'merged_{i}.pdf'
+        i += 1
+
+    # Write merged PDF to disk
+    merger.write(output_filename)
+    merger.close()
+
+    # Return merged PDF for download
+    return send_file(output_filename, as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
